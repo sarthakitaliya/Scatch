95e367054b7bf358194ffb4a8a1eb99572edcfe0
sarthak italiya <sarthak.italiya2005@gmail.com>
2024-08-17 17:16:59 +0530

    solve the erro at schema.js

--- schema.js
+++ schema.js
@@ -4,17 +4,17 @@ module.exports.productSchema = Joi.object({
     image: Joi.object({
         url: Joi.string().required(),
         filename: Joi.string().required(),
-    }).required,
+    }).required(),
     productname: Joi.string().required(),
     price: Joi.number().required().min(0),
     discount: Joi.number().default(0),
-    bgcolor: Joi.string().message('Color code is required').required(),
-    panelcolor: Joi.string().message('Color code is required').required(),
-    textcolor: Joi.string().message('Color code is required').required(),
+    bgcolor: Joi.string().required(),
+    panelcolor: Joi.string().required(),
+    textcolor: Joi.string().required(),
 })
 
 module.exports.signupSchema = Joi.object({
     username: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().min(8).required(),
 })
