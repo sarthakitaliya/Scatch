50c35a2c2ba6a13867cbcdea17e90fa7655a3883
sarthak italiya <sarthak.italiya2005@gmail.com>
2024-08-17 17:19:06 +0530

    solve the erro at schema.js

--- schema.js
+++ schema.js
@@ -4,7 +4,7 @@ module.exports.productSchema = Joi.object({
     image: Joi.object({
         url: Joi.string().required(),
         filename: Joi.string().required(),
-    }).required(),
+    }),
     productname: Joi.string().required(),
     price: Joi.number().required().min(0),
     discount: Joi.number().default(0),
